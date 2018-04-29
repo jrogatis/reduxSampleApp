e6484defc34b2f55783bbb3fe77ad48deb865a07
Jean Philip de Rogatis <jrogatis@metaconexao.com.br>
2018-04-29 10:18:35 -0300

    fix/lint

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BooksList from '../containers/books_list';
+import BookDetail from '../containers/book_detail';
 
 const App = () => (
   <div>
     <BooksList />
+    <BookDetail />
   </div>
 );
 export default App;
