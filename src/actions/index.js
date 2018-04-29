b46a636f820c59fe25011df7753b2817af175dea
Jean Philip de Rogatis <jrogatis@metaconexao.com.br>
2018-04-29 09:49:31 -0300

    add initial action

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+export const selectBook = book => ({ type: 'BOOK_SELECTED', payload: book });
--- src/containers/books_list.js
+++ src/containers/books_list.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions';
 
 class BooksList extends Component {
   renderList() {
     const { books } = this.props;
     return books.map(book => (
-      <li key={book.title} className="list-group-item">
+      <li key={book.title} onClick={() => this.props.selectBook(book)} className="list-group-item">
         {book.title}
       </li>
     ));
@@ -17,5 +19,6 @@ class BooksList extends Component {
 }
 
 const mapStateToProps = ({ books }) => ({ books });
+const mapDispatchToProps = dispatch => bindActionCreators({ selectBook }, dispatch);
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
