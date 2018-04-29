ab850380a121b599a7320324c939d17dbd77909f
Jean Philip de Rogatis <jrogatis@metaconexao.com.br>
2018-04-29 10:18:49 -0300

    fix/lint

---
+++ src/containers/book_detail.js
@@ -0,0 +1,18 @@
+import React, {
+  Component
+} from 'react';
+import {
+  connect
+} from 'react-redux';
+
+export default class BookDetail extends Component {
+  render() {
+    return <div > Book Detail! < /div>;
+  }
+}
+
+const mapStateToProps = ({
+  activeBook
+}) => ({
+  book: activeBook,
+});
